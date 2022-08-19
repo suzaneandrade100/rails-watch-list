ea192ba4345caaac8b0b7da566644247a6fc67a9
Suzane Andrade <suzaneandrade100@gmail.com>
2022-08-19 16:02:29 -0300

    saved

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:index, :new, :create]
   end
   resources :bookmarks, only: :destroy
 end
