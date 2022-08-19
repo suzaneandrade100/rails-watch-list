f775ad23bcd4e05409b6cfe5fd874a9387046a9c
Suzane Andrade <suzaneandrade100@gmail.com>
2022-08-19 16:40:33 -0300

    saved

--- Gemfile
+++ Gemfile
@@ -29,9 +29,13 @@ gem "jbuilder"
 
 
 gem "autoprefixer-rails"
+
 gem "font-awesome-sass", "~> 6.1"
+
 gem "simple_form", github: "heartcombo/simple_form"
+
 gem "sassc-rails"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,34 @@
 class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+     if @bookmark.save
+      redirect_to list_path(@list), notice: "Bookmark was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+
 end
